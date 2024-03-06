d7dafd0d725235d5233931ae047c734948502409
AgnikAm <KingaAleksandraKlima@gmail.com>
2024-03-06 17:20:02 +0100

    Create MyButton class

--- src/app.py
+++ src/app.py
@@ -1,47 +1,18 @@
 import flet as ft
+from buttons import MyButton
 
 def main(page: ft.Page) -> None:
     page.title = 'PhotoEditor'
     page.bgcolor = '#16171d'
-    page.window_max_height=1080
-    page.window_max_width=1920
-    page.padding=0
+    page.window_height = 1080
+    page.window_width = 1920
+    page.padding = 0
 
-    open_photo_btn = ft.OutlinedButton(
-        content=ft.Text(value='Open photo'),
-        style=ft.ButtonStyle(
-            bgcolor={ft.MaterialState.DEFAULT: '#374362',
-                     ft.MaterialState.HOVERED: '#1d3678'}
-        )
-    )
-
-    save_photo_btn = ft.OutlinedButton(
-        content=ft.Text(value='Save photo'),
-        style=ft.ButtonStyle(
-            bgcolor={ft.MaterialState.DEFAULT: '#374362',
-                     ft.MaterialState.HOVERED: '#1d3678'}
-        )
-    )
-
-    blur_btn = ft.FilledButton(
-        content=ft.Text(value='Blur'),
-        style=ft.ButtonStyle(
-            color='white',
-            bgcolor={ft.MaterialState.DEFAULT: '#374362',
-                     ft.MaterialState.HOVERED: '#1d3678'},
-            shape=ft.RoundedRectangleBorder(radius=5)
-        )
-    )
-
-    sharpen_btn = ft.FilledButton(
-        content=ft.Text(value='Sharpen'),
-        style=ft.ButtonStyle(
-            color='white',
-            bgcolor={ft.MaterialState.DEFAULT: '#374362',
-                     ft.MaterialState.HOVERED: '#1d3678'},
-            shape=ft.RoundedRectangleBorder(radius=5)
-        )
-    )
+    options_names = ['blur', 'sharpen', 'saturation', 'lighten', 'darken']
+    
+    open_photo = MyButton('Open photo')
+    save_photo = MyButton('Save photo')
+    path = ft.Text('No opened photo')
 
     navbar = ft.Container(
         width=page.window_max_width,
@@ -52,21 +23,22 @@ def main(page: ft.Page) -> None:
                                    margin=ft.margin.only(left=20)),
                       ft.Icon(ft.icons.ENHANCE_PHOTO_TRANSLATE_ROUNDED),
                       ft.VerticalDivider(width=20, thickness=2),
-                      open_photo_btn,
-                      save_photo_btn
-            ],          
+                      open_photo.build_file(),
+                      save_photo.build_file(),
+                      path
+            ],   
         ),
     )
 
-    edit_options = ft.ListView(expand=True, spacing=20, padding=20, auto_scroll=True)
-    edit_options.controls.append(blur_btn)
-    edit_options.controls.append(sharpen_btn)
+    edit_options = ft.ListView(expand=True, spacing=20, padding=20)
+    for option in options_names:
+        edit_options.controls.append(MyButton(option).build_option())
 
-    sidebar_menu = ft.Container(
+    sidebar = ft.Container(
         width=215,
-        height=page.window_max_height,
+        height=page.window_height,
         bgcolor='#202230',
-        content=edit_options
+        content=edit_options,
     )
 
     photo = ft.Container(
@@ -76,21 +48,17 @@ def main(page: ft.Page) -> None:
         border=ft.border.all(width=0.2, color='white'),
     )
 
-    wrapper = ft.Container(
-        width=1700,
+    canvas = ft.Container(
+        width=1720,
         height=1050,
         content=photo,
         alignment=ft.alignment.Alignment(0, -0.4)
     )
 
     workspace = ft.Container(
-        ft.Row(controls=[sidebar_menu, wrapper]),
-        margin=ft.margin.symmetric(vertical=-10),
+        ft.Row(controls=[sidebar, canvas]),
+         margin=ft.margin.symmetric(vertical=-10)
     )
 
     page.add(navbar)
     page.add(workspace)
-    
-
-
-
---
+++ src/buttons.py
@@ -0,0 +1,40 @@
+import flet as ft
+from typing import Callable
+
+class MyButton:
+    text: ft.Text
+    style: ft.ButtonStyle
+    onclick: Callable
+
+    def __init__(self, text: str) -> None:
+        self.text = ft.Text(value=text)
+        self.onclick = None
+
+    def define_onclick(self, fun: Callable) -> None:
+        self.onclick = fun
+
+    def build_file(self) -> ft.OutlinedButton:
+        self.style = ft.ButtonStyle(
+            bgcolor={ft.MaterialState.DEFAULT: '#374362',
+                     ft.MaterialState.HOVERED: '#1d3678'},
+        )
+
+        return ft.OutlinedButton(
+            content=self.text,
+            style=self.style,
+            on_click=self.onclick
+        )
+    
+    def build_option(self) -> ft.FilledButton:
+        self.style = ft.ButtonStyle(
+            color='white',
+            bgcolor={ft.MaterialState.DEFAULT: '#374362',
+                     ft.MaterialState.HOVERED: '#1d3678'},
+            shape=ft.RoundedRectangleBorder(radius=5)
+        )
+
+        return ft.FilledButton(
+            content=self.text,
+            style=self.style,
+            on_click=self.onclick
+        )
